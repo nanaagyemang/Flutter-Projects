99a1a064d8ca44f2f887e38244c0928f6fc1968b
nanaagyemang <nanaagyemang2058@gmail.com>
2022-06-16 13:17:21 +0000

    Initial Product Listing

---
+++ myproducts/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Airick\\Desktop\\productlistings\\myproducts" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\Airick\\Desktop\\productlistings\\myproducts"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Airick\\Desktop\\productlistings\\myproducts\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Airick\\Desktop\\productlistings\\myproducts"
+  "FLUTTER_TARGET=C:\\Users\\Airick\\Desktop\\productlistings\\myproducts\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Airick\\Desktop\\productlistings\\myproducts\\.dart_tool\\package_config.json"
+)
